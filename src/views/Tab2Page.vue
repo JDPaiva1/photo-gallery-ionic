b3715701a934cdb573db9e22a9066a5f06ab5bab
Jose Paiva <josedaniel@jdpaiva.me>
2023-01-28 20:04:15 +0000

    test for iOS and Android app

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -1,23 +1,72 @@
 <template>
-  <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
-        </ion-toolbar>
-      </ion-header>
+  <IonPage>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>Photo Gallery</IonTitle>
+      </IonToolbar>
+    </IonHeader>
 
-      <ExploreContainer name="Tab 2 page" />
-    </ion-content>
-  </ion-page>
+    <IonContent :fullscreen="true">
+      <IonGrid>
+        <IonRow>
+          <IonCol size="6" v-for="photo in photos" :key="photo.filepath">
+            <IonImg :src="photo.webviewPath" @click="showActionSheet(photo)"/>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+
+      <IonFab vertical="bottom" horizontal="center" slot="fixed">
+        <IonFabButton @click="takePhoto()">
+          <IonIcon :icon="camera"/>
+        </IonFabButton>
+      </IonFab>
+    </IonContent>
+  </IonPage>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {
+  actionSheetController,
+  IonPage,
+  IonHeader,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg
+} from '@ionic/vue';
+import { camera, trash, close } from 'ionicons/icons';
+import { usePhotoGallery, UserPhoto } from '@/composables/usePhotoGallery';
+
+const { photos, takePhoto, deletePhoto } = usePhotoGallery();
+
+const showActionSheet = async (photo: UserPhoto) => {
+  const actionSheet = await actionSheetController.create({
+    header: 'Photos',
+    buttons: [
+      {
+        text: 'Delete',
+        role: 'destructive',
+        icon: trash,
+        handler: () => {
+          deletePhoto(photo);
+        },
+      },
+      {
+        text: 'Cancel',
+        icon: close,
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+        },
+      },
+    ],
+  });
+  await actionSheet.present();
+};
 </script>
